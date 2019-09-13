04de662338feaa8dda5b5144d163575b28824273
Ubuntu <ubuntu@ip-172-31-27-176.ec2.internal>
2019-09-13 17:05:19 +0000

    basic math program

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Joey Marcotte
+# Created on: September 2019
+# This is the basic math program
+
+
+def main():
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}mm^2". format(5*3))
+    print("Perimeter is {}mm". format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
